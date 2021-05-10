c9ea087a8d4dc6e980434c15f6333753f3efd478
Denis Yordan <denis.yordan.p@gmail.com>
2021-05-10 22:17:36 +0700

    implements InstructionsFragment.kt

--- app/src/main/java/com/socialite/sores/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/socialite/sores/ui/fragments/instructions/InstructionsFragment.kt
@@ -1,11 +1,15 @@
 package com.socialite.sores.ui.fragments.instructions
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
 import com.socialite.sores.R
+import com.socialite.sores.models.Result
+import com.socialite.sores.ui.DetailsActivity
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -13,7 +17,25 @@ class InstructionsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val result = bundle
+        setWebView(view, result)
+
+        return view
+    }
+
+    private val bundle: Result?
+        get() {
+            val args = arguments
+            return args?.getParcelable(DetailsActivity.RESULT_BUNDLE_KEY)
+        }
+
+    private fun setWebView(view: View, result: Result?) {
+        view.instructions_webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl = result?.sourceUrl
+        if (!websiteUrl.isNullOrEmpty()) {
+            view.instructions_webView.loadUrl(websiteUrl)
+        }
     }
 }
