acbe003a22ca318499368b4a27cff382eaddf2d6
Denis Y Panggabean <denis.yordan.p@gmail.com>
2023-01-13 18:56:59 +0700

    setup app center

--- app/src/main/java/com/socialite/sores/MyApplication.kt
+++ app/src/main/java/com/socialite/sores/MyApplication.kt
@@ -1,8 +1,20 @@
 package com.socialite.sores
 
 import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCenter.start(
+            this, "2891441a-5325-4e83-9ea5-434bc56afbb7",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
 }
